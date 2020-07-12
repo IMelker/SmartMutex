8e05051c53b08432944d2232cefd6f3c5dd0e1cf
Igor Melker <l2pickn@yandex.ru>
2020-07-12 21:12:30 +0300

    project change structure

--- example/example.cpp
+++ example/example.cpp
@@ -7,20 +7,18 @@
 
 #include "smart_mutex.hpp"
 
-#define COUT_UNDERLINE() std::cout << "___________" << std::endl
-
 class XRayMutex : public std::mutex {
   public:
     XRayMutex() : std::mutex() {}
 
     void lock() {
-        std::cout << "Mutex(" << this << ") lock" << std::endl;
+        std::cout << "\t[Mutex/" << this << "] -  lock" << std::endl;
         std::mutex::lock();
     }
 
     void unlock() {
         std::mutex::unlock();
-        std::cout << "Mutex(" << this << ") unlock" << std::endl;
+        std::cout << "\t[Mutex/" << this << "] -  unlock" << std::endl;
     }
 };
 
@@ -29,43 +27,43 @@ int main() {
 
     // append() in critical section
     test->append("11");
-    COUT_UNDERLINE();
+    std::cout << std::endl;
 
     // c_str() in critical section
-    std::cout << test->c_str() << std::endl;
-    COUT_UNDERLINE();
+    std::cout << ">> " << test->c_str() << " EOF" << std::endl;
+    std::cout << std::endl;
 
 
     // scope lock for multiple operations
     {
         SmartMutex<std::string, XRayMutex>::ScopedAccess sa(test);
         sa->append("12");
-        std::cout << sa->c_str() << std::endl;
+        std::cout << ">> " << sa->c_str() << std::endl;
         sa->append("13");
-        std::cout << sa->c_str() << std::endl;
+        std::cout << ">> " << sa->c_str() << std::endl;
         sa->append("42");
-        std::cout << sa->c_str() << std::endl;
+        std::cout << ">> " << sa->c_str() << std::endl;
     }
-    COUT_UNDERLINE();
+    std::cout << std::endl;
 
     // copy constructor
     SmartMutex<std::string, XRayMutex> test_copy("1211121342");
 
     if(test_copy == test) {
-        std::cout << "Data is equal" << std::endl;
+        std::cout << ">> Data is equal" << std::endl;
     }
-    COUT_UNDERLINE();
+    std::cout << std::endl;
     test_copy->append("11");
-    COUT_UNDERLINE();
+    std::cout << std::endl;
 
     if(test_copy != test) {
-        std::cout << "Data is not equal" << std::endl;
+        std::cout << ">> Data is not equal" << std::endl;
     }
-    COUT_UNDERLINE();
+    std::cout << std::endl;
 
     std::string testStr = test_copy;
     std::cout << testStr << std::endl;
-    COUT_UNDERLINE();
+    std::cout << std::endl;
 }
 
 
